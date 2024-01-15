c17cef9676db25cb0684297c966a7fecb6fe6e2f
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2024-01-15 23:23:49 +0530

    added new project

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -23,6 +23,34 @@ export default function ProjectsPage(): JSX.Element {
 			),
 			language: ['Next.js 13', 'GraphQL', 'Apollo Client', 'Tailwind CSS', 'Typescript'],
 		},
+
+
+		{
+			name: 'ShoeVista (E-commerce Website)',
+			description:
+				'By using this website store, users can choose their favourite footware, and to process the order.',
+			url: 'https://github.com/Shivam00002/shoevista',
+			homepage: 'https://shoevista.vercel.app/',
+			icon: (
+				<Image
+					src={'https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNob2VzfGVufDB8fDB8fHww'}
+					height={48}
+					width={48}
+				/>
+			),
+			language: [
+				
+				'Next.js 13',
+				'TypeScript',
+				'Tailwind css',
+				
+			],
+		},
+
+
+
+
+
 		{
 			name: 'Bigbasket-Clone (E-commerce Website)',
 			description:
@@ -47,6 +75,11 @@ export default function ProjectsPage(): JSX.Element {
 				'ChakraUi',
 			],
 		},
+
+
+
+
+
 		{
 			name: 'Aqua-shop (E-commerce Website)',
 			description:
